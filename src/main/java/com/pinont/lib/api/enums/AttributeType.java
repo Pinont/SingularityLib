f0d269dde072b44e141355a1858bcd40053eaa4f
PinozenTH <pinozen.thailand@gmail.com>
2025-04-11 19:13:56 +0700

    Fix Attribute Name

--- src/main/java/com/pinont/lib/api/enums/AttributeType.java
+++ src/main/java/com/pinont/lib/api/enums/AttributeType.java
@@ -4,18 +4,18 @@ import org.bukkit.attribute.Attribute;
 
 
 public enum AttributeType {
-    pluginMAX_HEALTH("generic.max_health"),
-    pluginMAX_ABSORPTION("generic.max_absorption"),
-    pluginFOLLOW_RANGE("generic.follow_range"),
-    pluginKNOCKBACK_RESISTANCE("generic.knockback_resistance"),
-    pluginMOVEMENT_SPEED("generic.movement_speed"),
-    pluginATTACK_DAMAGE("generic.attack_damage"),
-    pluginARMOR("generic.armor"),
-    pluginARMOR_TOUGHNESS("generic.armor_toughness"),
-    pluginATTACK_KNOCKBACK("generic.attack_knockback"),
-    pluginATTACK_SPEED("generic.attack_speed"),
-    pluginLUCK("generic.luck"),
-    pluginFLYING_SPEED("generic.flying_speed"),
+    MAX_HEALTH("generic.max_health"),
+    MAX_ABSORPTION("generic.max_absorption"),
+    FOLLOW_RANGE("generic.follow_range"),
+    KNOCKBACK_RESISTANCE("generic.knockback_resistance"),
+    MOVEMENT_SPEED("generic.movement_speed"),
+    ATTACK_DAMAGE("generic.attack_damage"),
+    ARMOR("generic.armor"),
+    ARMOR_TOUGHNESS("generic.armor_toughness"),
+    ATTACK_KNOCKBACK("generic.attack_knockback"),
+    ATTACK_SPEED("generic.attack_speed"),
+    LUCK("generic.luck"),
+    FLYING_SPEED("generic.flying_speed"),
     JUMP_STRENGTH("horse.jump_strength"),
     SPAWN_REINFORCEMENTS("zombie.spawn_reinforcements"),
     ;
@@ -28,18 +28,18 @@ public enum AttributeType {
 
     public Attribute getAttribute() {
         return switch (this) {
-            case pluginMAX_HEALTH -> Attribute.MAX_HEALTH;
-            case pluginMAX_ABSORPTION -> Attribute.MAX_ABSORPTION;
-            case pluginFOLLOW_RANGE -> Attribute.FOLLOW_RANGE;
-            case pluginKNOCKBACK_RESISTANCE -> Attribute.KNOCKBACK_RESISTANCE;
-            case pluginMOVEMENT_SPEED -> Attribute.MOVEMENT_SPEED;
-            case pluginATTACK_DAMAGE -> Attribute.ATTACK_DAMAGE;
-            case pluginARMOR -> Attribute.ARMOR;
-            case pluginARMOR_TOUGHNESS -> Attribute.ARMOR_TOUGHNESS;
-            case pluginATTACK_KNOCKBACK -> Attribute.ATTACK_KNOCKBACK;
-            case pluginATTACK_SPEED -> Attribute.ATTACK_SPEED;
-            case pluginLUCK -> Attribute.LUCK;
-            case pluginFLYING_SPEED -> Attribute.FLYING_SPEED;
+            case MAX_HEALTH -> Attribute.MAX_HEALTH;
+            case MAX_ABSORPTION -> Attribute.MAX_ABSORPTION;
+            case FOLLOW_RANGE -> Attribute.FOLLOW_RANGE;
+            case KNOCKBACK_RESISTANCE -> Attribute.KNOCKBACK_RESISTANCE;
+            case MOVEMENT_SPEED -> Attribute.MOVEMENT_SPEED;
+            case ATTACK_DAMAGE -> Attribute.ATTACK_DAMAGE;
+            case ARMOR -> Attribute.ARMOR;
+            case ARMOR_TOUGHNESS -> Attribute.ARMOR_TOUGHNESS;
+            case ATTACK_KNOCKBACK -> Attribute.ATTACK_KNOCKBACK;
+            case ATTACK_SPEED -> Attribute.ATTACK_SPEED;
+            case LUCK -> Attribute.LUCK;
+            case FLYING_SPEED -> Attribute.FLYING_SPEED;
             case JUMP_STRENGTH -> Attribute.JUMP_STRENGTH;
             case SPAWN_REINFORCEMENTS -> Attribute.SPAWN_REINFORCEMENTS;
         };
