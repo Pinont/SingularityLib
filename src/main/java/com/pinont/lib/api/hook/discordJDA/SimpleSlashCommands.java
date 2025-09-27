cee8f21b5b778941b6a488a23522f7e4c336ee1a
PinozenTH <pinozen.thailand@gmail.com>
2025-09-27 19:59:19 +0700

    Add DiscordApp and related classes for JDA integration
    Signed-off-by: PinozenTH <pinozen.thailand@gmail.com>

---
+++ src/main/java/com/pinont/lib/api/hook/discordJDA/SimpleSlashCommands.java
@@ -0,0 +1,7 @@
+package com.pinont.lib.api.hook.discordJDA;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public abstract class SimpleSlashCommands extends ListenerAdapter {
+    public abstract SlashCommandComponent getCommand();
+}
