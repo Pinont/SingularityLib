e988185e098f99a10f8bf1759ecb2a9ecad544c1
Nont Nonnipat <pinozen.thailand@gmail.com>
2025-10-19 00:56:55 +0700

    feat: add EntityDamageListener and PlayerDamageByPlayerEvent for player damage handling

---
+++ src/main/java/com/github/pinont/singularitylib/plugin/listener/EntityDamageListener.java
@@ -0,0 +1,21 @@
+package com.github.pinont.singularitylib.plugin.listener;
+
+import com.github.pinont.singularitylib.api.annotation.AutoRegister;
+import com.github.pinont.singularitylib.api.event.PlayerDamageByPlayerEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+@AutoRegister
+public class EntityDamageListener implements Listener {
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageByEntityEvent event) {
+        if (event.getEntity() instanceof Player player && event.getDamager() instanceof Player damager) {
+            PlayerDamageByPlayerEvent playerDamageEvent = new PlayerDamageByPlayerEvent(player, damager);
+            event.setCancelled(playerDamageEvent.callEvent());
+        }
+    }
+
+}
